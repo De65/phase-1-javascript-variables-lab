44fb33041cf59f27d982e9f5559a91b08a14d92e
De65 <kyalodiana6@gmail.com>
2023-03-14 12:27:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 let companyName = 'Scuber';
-function(){
-const companyName = 'Scuber';
-	console.log(companyName);
-}
-if mostProfitableNeighborhood = 'Chelsea';
 let mostProfitableNeighborhood = 'Chelsea';
-
-if companyCeo = 'SusanSmith';
-let companyCeo ='SusanSmith';
+let companyCeo = 'Susan Smith';
